6bc8ee8d34c46814713bc9b478a175cfe4d71945
vra000 <voronin.a.romanvra@mail.ru>
2026-09-21 22:18:41 +0300

    Незначительные изменения

--- mosdiary/__init__.py
+++ mosdiary/__init__.py
@@ -1,3 +1,7 @@
+"""Неофициальная асинхронная библиотека для работы с API школьного дневника МЭШ."""
+
 from .client import MOSDiaryClient
 
-__all__ = ('MOSDiaryClient')
+__version__ = "0.2.0"
+
+__all__ = ("MOSDiaryClient", "__version__")
